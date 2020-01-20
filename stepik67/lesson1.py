aa0dab27a9f1bc8841a576323f8d65ec68d3b10f
Alena Piskunova <alena.prideina@yandex.ru>
2020-01-21 00:14:27 +0700

    Рефакторинг: переименование файлов, иначе подключается модуль в юнит-тестах

--- stepik67/lesson1.py
+++ stepik67/lesson1.py
@@ -1,21 +1,32 @@
-import stepik67
-
-print("Все в рамках одних суток")
-print("Введите количество часов и минут. В ответ получите общее количество минут")
-x = int(input())
-y = int(input())
-print(stepik67.minutes_from_hm(x, y))
-
-print("Введите количество минут. В ответ получите количество часов и минут")
-m = int(input())
-for x in stepik67.minutes_to_hm(m):
-    print(x)
+def minutes_from_hm(x, y):
+    return x*60 + y
 
 
-print("Введите количество минут, затем часы и минуты, к которым их нужно прибавить. Ответ - часы, минуты")
-X = int(input())
-H = int(input())
-M = int(input())
+def minutes_to_hm(m):
+    return m // 60, m % 60
 
-for x in stepik67.hm_plus_minutes_to_hm(X, H, M):
-    print(x)
+
+def hm_plus_minutes_to_hm(x, h, m):
+    return ((h * 60 + m) + x) // 60, ((h * 60 + m) + x) % 60
+
+
+if __name__ == '__main__':
+    print("Все в рамках одних суток")
+    print("Введите количество часов и минут. В ответ получите общее количество минут")
+    x = int(input())
+    y = int(input())
+    print(minutes_from_hm(x, y))
+
+    print("Введите количество минут. В ответ получите количество часов и минут")
+    m = int(input())
+    for x in minutes_to_hm(m):
+        print(x)
+
+    print("Введите количество минут, затем часы и минуты, к которым их нужно прибавить. Ответ - часы, минуты")
+    X = int(input())
+    H = int(input())
+    M = int(input())
+
+    for x in hm_plus_minutes_to_hm(X, H, M):
+        print(x)
+
--- tests/test_stepik.py
+++ tests/test_stepik.py
@@ -1,19 +1,19 @@
 import unittest
-import stepik67
+import stepik67.lesson1 as L1
 
 
 class MyTest(unittest.TestCase):
     def test_minutesToHM_1(self):
-        self.assertEqual(stepik67.minutes_from_hm(7, 30), 450)
+        self.assertEqual(L1.minutes_from_hm(7, 30), 450)
 
     def test_minutesToHM_2(self):
-        self.assertEqual(stepik67.minutes_from_hm(0, 42), 42)
+        self.assertEqual(L1.minutes_from_hm(0, 42), 42)
 
     def test_minutes_to_hm_1(self):
-        self.assertEqual(stepik67.minutes_to_hm(480), (8, 0))
+        self.assertEqual(L1.minutes_to_hm(480), (8, 0))
 
     def test_minutes_to_hm_2(self):
-        self.assertEqual(stepik67.minutes_to_hm(512), (8, 32))
+        self.assertEqual(L1.minutes_to_hm(512), (8, 32))
 
 
 if __name__ == "__main__":
